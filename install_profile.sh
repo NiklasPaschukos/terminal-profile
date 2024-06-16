9bb8fdecad2e15786c271372a4176334185dbe52
Niklas <niklaspaschukos@gmail.com>
2024-06-16 22:49:01 +0200

    uncommented color theme for now

--- install_profile.sh
+++ install_profile.sh
@@ -15,19 +15,19 @@ sudo cp configs/pixegami-agnoster.zsh-theme ~/.oh-my-zsh/themes/pixegami-agnoste
 dconf load /org/gnome/terminal/legacy/profiles:/:fb358fc9-49ea-4252-ad34-1d25c649e633/ < configs/terminal_profile.dconf
 
 # Add it to the default list in the terminal
-add_list_id=fb358fc9-49ea-4252-ad34-1d25c649e633
-old_list=$(dconf read /org/gnome/terminal/legacy/profiles:/list | tr -d "]")
+#add_list_id=fb358fc9-49ea-4252-ad34-1d25c649e633
+#old_list=$(dconf read /org/gnome/terminal/legacy/profiles:/list | tr -d "]")
 
-if [ -z "$old_list" ]
-then
-	front_list="["
-else
-	front_list="$old_list, "
-fi
+#if [ -z "$old_list" ]
+#then
+#	front_list="["
+#else
+#	front_list="$old_list, "
+#fi
 
-new_list="$front_list'$add_list_id']"
-dconf write /org/gnome/terminal/legacy/profiles:/list "$new_list" 
-dconf write /org/gnome/terminal/legacy/profiles:/default "'$add_list_id'"
+#new_list="$front_list'$add_list_id']"
+#dconf write /org/gnome/terminal/legacy/profiles:/list "$new_list" 
+#dconf write /org/gnome/terminal/legacy/profiles:/default "'$add_list_id'"
 
 # Switch the shell.
 chsh -s $(which zsh)
